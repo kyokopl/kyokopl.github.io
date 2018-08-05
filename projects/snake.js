0d3ebef0c38f5a7219b3e140b0c5e8d993962ea6
Kyoko Pohe <Kyokz@Kyokos-MacBook-Pro.local>
2018-08-05 20:42:46 +1200

    final touches on project

--- projects/snake.js
+++ projects/snake.js
@@ -1,41 +1,7 @@
-
-function start () {
-  black()
-  yellow()
-  three()
-  four()
+function setPixelColor(pixel) {
+  pixel.style.backgroundColor = 'yellow';
 }
 
-function black () {
-  var black = document.getElementsByClassName('black')
-  black.addEventListener('click', makeBlue)
-  black.addEventListener('dblclick', makeWhite)
-  black.addEventListener('contextmenu', makeBlack)
-}
-
-function yellow () {
-  var yellow = document.getElementById('yellow')
-  black.addEventListener('click', makeBlue)
-  black.addEventListener('dblclick', makeWhite)
-}
-
-// Changes the background color of event's target
-function makeBlack (evt) {
-  evt.target.style.backgroundColor = 'blue'
-}
-
-function makeGreen (evt) {
-  evt.target.style.backgroundColor = 'green'
-}
-
-function makeOrange (evt) {
-  evt.target.style.backgroundColor = 'orange'
-}
-
-function makeBlack (evt) {
-  evt.target.style.backgroundColor = 'black'
-}
-
-function makeWhite (evt) {
-  evt.target.style.backgroundColor = 'white'
+function setPixelBlack(pixel) {
+  pixel.style.backgroundColor = 'black';
 }
